52df2485cc6414897a1ad7042059dd433a605f61
Dileep2896 <dileepkumar2896@gmail.com>
2024-07-16 15:39:22 -0400

    updated word cloud

--- src/components/WordCloudComponent.jsx
+++ src/components/WordCloudComponent.jsx
@@ -1,17 +1,15 @@
 // WordCloudComponent.js
-import WordCloud from "react-wordcloud";
+import WordCloud from "react-d3-cloud";
 
 const words = [
   { text: "Software", value: 1000 },
   { text: "Developement", value: 950 },
   { text: "Engineer", value: 1000 },
-
   { text: "Java", value: 800 },
   { text: "JavaScript", value: 850 },
   { text: "Python", value: 700 },
   { text: "Kotlin", value: 650 },
   { text: "C++", value: 600 },
-
   { text: "React", value: 550 },
   { text: "Node", value: 500 },
   { text: "Express", value: 500 },
@@ -24,22 +22,15 @@ const words = [
   { text: "PostgreSQL", value: 150 },
 ];
 
-const options = {
-  rotations: 2,
-  rotationAngles: [-90, 0],
-  fontSizes: [20, 60],
-  fontFamily: "Fira Code",
-  scale: "sqrt",
-  spiral: "archimedean",
-  transitionDuration: 1000,
-  callbacks: {
-    onWordClick: null,
-    getWordTooltip: (word) => `${word.text}`, // Optional: To show tooltip with word text only
-  },
-};
+const fontSizeMapper = (word) => Math.log2(word.value) * 5;
+const rotate = () => (Math.random() > 0.5 ? 0 : 90);
 
 const WordCloudComponent = () => {
-  return <WordCloud words={words} options={options} />;
+  return (
+    <div>
+      <WordCloud data={words} fontSizeMapper={fontSizeMapper} rotate={rotate} />
+    </div>
+  );
 };
 
 export default WordCloudComponent;
