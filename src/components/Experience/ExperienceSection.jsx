2dfec47ab1c989ca6eda94c901a4c7df3fd66c8f
Dileep2896 <dileepkumar2896@gmail.com>
2024-07-17 16:17:00 -0400

    Added Experience

--- src/components/Experience/ExperienceSection.jsx
+++ src/components/Experience/ExperienceSection.jsx
@@ -1,11 +1,64 @@
-import { Container } from "react-bootstrap";
 import { Element } from "react-scroll";
 import "./experience.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { timelineElements } from "../../constants/timelineElements";
+import "react-vertical-timeline-component/style.min.css";
+import { Container } from "react-bootstrap";
 
 function ExperienceSection() {
   return (
     <Element name="experience-section" className="exp-section">
-      <Container></Container>
+      <h2
+        style={{ paddingBottom: "20px" }}
+        className="subsection-heading exp-heading"
+      >
+        INTERNSHIP EXPERIENCE
+      </h2>
+      <VerticalTimeline>
+        {timelineElements.map((elements) => {
+          return (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              key={elements.id}
+              contentStyle={{
+                background: "#2C2E31",
+                color: "#fff",
+                textAlign: "justify",
+              }}
+              iconStyle={{ background: "#00c7a0", color: "#fff" }}
+              date={elements.date}
+              dateClassName="timelineDate"
+            >
+              <h3 className="vertical-timeline-element-title">
+                {elements.title}
+              </h3>
+              <h5 className="vertical-timeline-element-subtitle">
+                {elements.location}
+              </h5>
+              <Container
+                style={{
+                  marginTop: "10px",
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: "5px",
+                }}
+              >
+                {elements.skills.map((skill, i) => {
+                  return (
+                    <span key={i} id="exp-skill">
+                      {skill}
+                    </span>
+                  );
+                })}
+              </Container>
+              <p id="description">{elements.description}</p>
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
     </Element>
   );
 }
---
+++ src/constants/timelineElements.jsx
@@ -0,0 +1,60 @@
+export let timelineElements = [
+  {
+    id: 1,
+    title: "CEO and Founder",
+    location: "Bengaluru, India",
+    description:
+      "Evaluated a Flutter-based EV charging app, analyzed AI algorithms reducing wait times by 60% and achieving compatibility with 20+ models, demonstrated excellent debugging and problem-solving skills in a fast-paced environment, and converted comprehensive requirements into actionable tasks, collaborating with cross-functional teams for high-quality solutions.",
+    date: "August 2022 - July 2023",
+    skills: ["Flutter", "Leadership", "Problem Solving", "Entrepreneurship"],
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Flutter Developer Inten",
+    location: "Bengaluru, India",
+    description:
+      "Enhanced a chess game app with Chess.com APIs using BLOC architecture for efficient state management, engineered and examined 2 strategy game apps integrating Unity in Flutter using Dart, and evaluated client needs through detailed surveys and feedback sessions.",
+    date: "January 22 - April 22",
+    skills: ["Flutter", "Firebase", "BLOC", "Dart", "Unity", "Provider"],
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "Android App Developer Intern",
+    location: "Bengaluru, India",
+    description:
+      "Engineered a Java-based Android app for live badminton data with login to CRUD operations, integrated a Wi-Fi camera for 10-second video feeds using RxJava and MPAndroidChart, and leveraged RESTful APIs, Room, and LiveData for real-time data and state management, enhancing user engagement by 40%, performance by 30%, and user experience by 25%.",
+    date: "July 2021 - November 2021",
+    skills: [
+      "Java",
+      "Android Studio",
+      "MVVM",
+      "RxJava",
+      "AWS",
+      "RESTful APIs",
+      "Provider",
+      "Gradle",
+      "Maven",
+    ],
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "Full Stack Engineering Intern",
+    location: "Bengaluru, India",
+    description:
+      "Improved profile approvals, security, and permissions control with Node.js and AWS, reducing admin workload by 40%, engineered Node.js/Express RESTful APIs with Elasticsearch for a 25% improvement in search efficiency and user matching, and maintained, debugged, and redesigned the website with Material-UI for enhanced stability and aesthetics.",
+    date: "July 2021 - November 2021",
+    skills: [
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "ReactJS",
+      "NodeJS",
+      "AWS",
+      "Material-UI",
+    ],
+    icon: "work",
+  },
+];
